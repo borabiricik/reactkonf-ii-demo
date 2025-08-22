cd6f86cb3427a5a7165a67f285a30e65d337bbca
Bora Biricik <bora.bir496@gmail.com>
2025-08-22 15:39:54 +0300

    feat: basic optimizations added

---
+++ src/app/basics/optimizations/page.tsx
@@ -0,0 +1,63 @@
+import { Fira_Code } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+const Optimizations = () => {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Outfit:wght@100..900&display=swap"
+        rel="stylesheet"
+      />
+      <div className={`flex flex-col gap-4`}>
+        <h1 className="text-4xl font-bold text-center">Optimizations</h1>
+        <div className="px-16">
+          <h2 className="text-2xl font-bold">1. Image Optimization</h2>
+          <div className="flex items-center gap-4 mt-4">
+            <div>
+              <h3 className="text-lg font-bold">With next/image</h3>
+              <Image
+                src="https://picsum.photos/200"
+                alt="Image Optimization"
+                width={200}
+                height={200}
+              />
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Without next/image</h3>
+              <img src="https://picsum.photos/200" alt="Image Optimization" />
+            </div>
+          </div>
+        </div>
+        <div className="px-16">
+          <h2 className="text-2xl font-bold">2. Font Optimization</h2>
+          <div className="grid grid-cols-2 w-3/4 gap-4 mt-4">
+            <div className="flex flex-col gap-2">
+              <h3 className="text-lg font-bold">With next/font</h3>
+              <p className={`${firaCode.className}`}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quos.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h3 className="text-lg font-bold">Without next/font</h3>
+              <p style={{ fontFamily: "Fira Code" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quos.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Optimizations;
---
+++ src/app/basics/page.tsx
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Basics() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
+      <header className="mt-[200px] flex flex-col items-center gap-4">
+        <h1 className="text-4xl font-bold flex flex-col items-center gap-4">
+          <span>Who you should pick</span>
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={320}
+            height={100}
+            priority
+          />
+          <span>for Your Next Startup?</span>
+        </h1>
+        <div>-</div>
+        <div>23.08.2025</div>
+        <div>-</div>
+        <div className="text-5xl font-bold">Basics</div>
+      </header>
+      <main>
+        <ol className="font-mono font-medium flex flex-col gap-2 text-left w-full">
+          <li>
+            <Link href={"/basics/optimizations"} className="hover:underline">
+              <code>1. Optimizations</code>
+            </Link>
+          </li>
+          <Link href={"/basics/caching"} className="hover:underline">
+            <li>
+              <code>2. Caching</code>
+            </li>
+          </Link>
+          <Link href={"/basics/routing"} className="hover:underline">
+            <li>
+              <code>3. Routing</code>
+            </li>
+          </Link>
+          <Link href={"/basics/seo"} className="hover:underline">
+            <li>
+              <code>4. SEO</code>
+            </li>
+          </Link>
+          <Link href={"/basics/api-routes"} className="hover:underline">
+            <li>
+              <code>5. API Routes</code>
+            </li>
+          </Link>
+        </ol>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
